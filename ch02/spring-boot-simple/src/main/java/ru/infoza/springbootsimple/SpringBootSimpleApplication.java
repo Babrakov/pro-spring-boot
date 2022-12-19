5b3a427daa454a3e919e594f06d7b856bb927a41
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-12-19 20:55:08 +0100

    Some tricks with banner

--- ch02/spring-boot-simple/src/main/java/ru/infoza/springbootsimple/SpringBootSimpleApplication.java
+++ ch02/spring-boot-simple/src/main/java/ru/infoza/springbootsimple/SpringBootSimpleApplication.java
@@ -1,13 +1,24 @@
 package ru.infoza.springbootsimple;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class SpringBootSimpleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSimpleApplication.class, args);
+		SpringApplication app = new SpringApplication(SpringBootSimpleApplication.class);
+//		app.setBanner(new Banner() {
+//			@Override
+//			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+//				out.println("\n\nThis is my own banner!\n\n".toUpperCase());
+//			}
+//		});
+		app.run(args);
 	}
 
 }
