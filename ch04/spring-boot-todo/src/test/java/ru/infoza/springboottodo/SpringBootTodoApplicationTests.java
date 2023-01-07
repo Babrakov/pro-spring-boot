c00bfe7d7301318974f1c12ba8d84cbe0f054d4d
Sergei Babrakov <s.a.babrakov@gmail.com>
2023-01-07 16:45:03 +0100

    Added chapter 3 and 4

---
+++ ch04/spring-boot-todo/src/test/java/ru/infoza/springboottodo/SpringBootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.infoza.springboottodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
