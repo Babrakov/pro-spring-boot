d82cba1e45ef8902e2aec659a22bc3c6e89605db
Sergei Babrakov <s.a.babrakov@gmail.com>
2023-01-31 20:07:14 +0100

    Added simple spring boot app with jdbc template

---
+++ ch05/todo-jdbc/src/test/java/ru/infoza/todojdbc/TodoJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.infoza.todojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
